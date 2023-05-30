55f4f441ac287b5b4b69bc3323e4f05c57138f3c
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-05-30 22:52:53 +0100

    router

--- 03-task-manager/app.js
+++ 03-task-manager/app.js
@@ -11,5 +11,5 @@ app.use('/api/v1/tasks', tasks)
 
 const port = 3000
 app.listen(port, ()=>{
-    console.log(`Server is listening on port ${port}`);
+    console.log(`Server is listening successfully on port ${port}`);
 })
