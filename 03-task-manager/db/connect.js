06415026746b3b0576d2f49ee62758db624cfa17
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-06-02 08:26:57 +0100

    mongoDb setting

--- 03-task-manager/db/connect.js
+++ 03-task-manager/db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = "mongodb+srv://Babykoko:V2lRAoKiym2TqoUg@cluster0.1gqmign.mongodb.net/Task-Manager?retryWrites=true&w=majority"
+const connectionString = ""
 
 const connectDb = (url) => {
    return mongoose.connect(connectionString)
