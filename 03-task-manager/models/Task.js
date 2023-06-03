fcd9e5cf6409fba44ac7b8e9476b993c2f3b9ec6
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-06-03 21:15:57 +0100

    crud operation

--- 03-task-manager/models/Task.js
+++ 03-task-manager/models/Task.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-     name : String,
+     name : {
+        type : String,
+        required : true
+     },
      completed : Boolean
 })
 
