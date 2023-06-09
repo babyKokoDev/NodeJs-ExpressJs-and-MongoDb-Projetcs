d078ca6812ffc15fb38772e3c758fb22b27b793b
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-06-09 09:17:52 +0100

    get all tasks

--- 03-task-manager/controllers/tasks.js
+++ 03-task-manager/controllers/tasks.js
@@ -1,16 +1,26 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req, res) => {
-    res.send('Get all tasks')
+const getAllTasks = async (req, res) => {
+    try {
+       const task = await Task.find({}) 
+       res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+    
 }
 
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body)
-    res.status(201).json({task})
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
 }
 
 const getTask = (req, res) => {
-   res.json({ id : req.params.id})
+    res.json({ id: req.params.id })
 }
 
 const updateTasks = (req, res) => {
