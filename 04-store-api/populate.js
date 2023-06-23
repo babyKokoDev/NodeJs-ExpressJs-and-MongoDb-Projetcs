4e888f2afadc54c727c6b7ca67bf2d2faa425c05
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-06-23 17:14:18 +0100

    data population to database

---
+++ 04-store-api/populate.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const connectDB = require('./db/connect')
+const Product = require('./models/product')
+const jsonProduct = require('./products.json')
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        await Product.deleteMany()
+        await Product.create(jsonProduct)
+        console.log('success...')
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
+
+
