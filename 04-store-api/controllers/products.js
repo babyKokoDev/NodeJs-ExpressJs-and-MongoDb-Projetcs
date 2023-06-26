54f7d64ca2287b540063e20ab4b912c490dca2c3
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-06-26 11:46:57 +0100

    filterng

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -8,8 +8,16 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured } = req.query
-    const products = await Product.find(req.query)
+    const { featured, company } = req.query
+    const queryObject = {}
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    if (company) {
+        queryObject.company = company
+    }
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
     res.status(200).json({products, nbHits : products.length})
 }
 
